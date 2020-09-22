0be166c575f52bc2e0a1e20c4a5ab3f5f2b0dc52
Md.Asif Ur Rahman <45636891+asifurrahman01714@users.noreply.github.com>
2020-09-22 07:39:32 +0600

    Add files via upload

---
+++ main23.js
@@ -0,0 +1,9 @@
+function addstyle(){
+    var myid = document.getElementById("headid");
+    myid.classList.add("heading");
+}
+
+function removestyle(){
+    var myid = document.getElementById("headid");
+    myid.classList.remove("heading");
+}
